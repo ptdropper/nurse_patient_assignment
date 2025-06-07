886af32001237efecec6e07673e15328dbaa17a6
Robert Smigielski <ptdropper@gmail.com>
2025-06-07 12:31:08 -0400

    WIP heroku integration for web access

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn interface.web:app
--- interface/web.py
+++ interface/web.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
+import os
 from scheduler.solver import load_data
 from scheduler.scheduler import assign_nurses_to_patients
 from scheduler.utils import save_schedule
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, static_folder='../static', template_folder="../templates")
+
+# Serve static HTML/CSS
+@app.route('/')
+def serve_index():
+    return send_from_directory('../templates', 'index.html')
+
+# Serve other static files
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('../static', path)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -40,6 +51,10 @@ def index():
 
 if __name__ == "__main__":
     # auto reload for convenience but breaks PyCharm debugger
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    port = int(os.environ.get('PORT', 5000))  # Use Heroku's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port)
+
     # provide for PyCharm debugging by telling Flask to not use reloader
     #app.run(debug=True, use_reloader=False)
