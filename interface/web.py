f8f1a55d952eca8f0c1dba6e57e79370dd623180
Robert Smigielski <ptdropper@gmail.com>
2025-06-07 13:21:02 -0400

    WIP heroku integration for web access

--- interface/web.py
+++ interface/web.py
@@ -4,12 +4,12 @@ from scheduler.solver import load_data
 from scheduler.scheduler import assign_nurses_to_patients
 from scheduler.utils import save_schedule
 
-app = Flask(__name__, static_folder='../static', template_folder="../templates")
+app = Flask(__name__, static_folder='../static')
 
 # Serve static HTML/CSS
 @app.route('/')
 def serve_index():
-    return send_from_directory('../templates', 'index.html')
+    return send_from_directory('../static', 'index.html')
 
 # Serve other static files
 @app.route('/<path:path>')
